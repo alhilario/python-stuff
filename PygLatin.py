e5464cfcb9231d43e2bf3ba69a6a01912cf22752
Alvaro Hilario <king314@gmail.com>
2013-09-09 07:29:45 -0700

    Update PygLatin.py

--- PygLatin.py
+++ PygLatin.py
@@ -10,9 +10,8 @@ if len(original) > 0 and original.isalpha():
     first = word[0]
     if (first == 'a' or first == 'e' or first == 'i' or first == 'o' or first == 'u'):
         new_word = word + pyg
-        print new_word
     else:
         new_word = word[1:] + word[0] + pyg
-        print new_word
+    print new_word
 else:
     print 'empty'
