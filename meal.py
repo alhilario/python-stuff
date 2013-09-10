0e2fd26ff9a81f35a169033f0557dbc0a0b40b9b
Alvaro Hilario <king314@gmail.com>
2013-09-10 08:23:50 -0700

    Update meal.py

--- meal.py
+++ meal.py
@@ -1,11 +1,18 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-meal = 44.50
-tax = 0.0675
-tip = 0.15
+def tax(bill):
+    """Adds 8% tax to a restaurant bill."""
+    bill *= 1.08
+    print "With tax: %f" % bill
+    return bill
 
-meal = meal + meal * tax
-total = meal + meal * tip
-
-print("%.2f" % total)
+def tip(bill):
+    """Adds 15% tip to a restaurant bill."""
+    bill *= 1.15
+    print "With tip: %f" % bill
+    return bill
+    
+meal_cost = 100
+meal_with_tax = tax(meal_cost)
+meal_with_tip = tip(meal_with_tax)
